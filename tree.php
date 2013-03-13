a5a16cdbf4d1a1edefa0739aa7b9138a82dd1ddd
unknown <kuday@sina.com>
2013-03-13 19:23:33 +0800

    first commit

---
+++ tree.php
@@ -0,0 +1,59 @@
+<?php
+class Tree
+{
+
+	public function __construct()
+	{
+	
+	
+	}
+	
+	public function getChild($id)
+	{
+	
+	
+	
+	}
+	
+	
+	public function getParent($id)
+	{
+	
+	
+	}
+	
+	public function getTree($id)
+	{
+	
+	
+	
+	}
+	
+	public function getNext($id)
+	{
+	
+	
+	}
+	
+	public function getPrevious($id)
+	{
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+}
